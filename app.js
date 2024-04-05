636ca8e80ad9aae3fab5e02bb3710a310c870b42
annc19324 <161805046+annc19324@users.noreply.github.com>
2024-04-05 20:33:15 +0700

    Update app.js

--- app.js
+++ app.js
@@ -18,7 +18,199 @@ const html = `
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BEM</title>
     <link rel="stylesheet" href="BEM.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <style>
+    * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+}
+  
+body {
+    
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    background-color: #f4f4f5;
+    font-family: 'Times New Roman';
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+    
+}
+
+body > div {
+    margin: auto;  
+}
+
+#toast {
+    position: fixed;
+    top: 32px;
+    right: 32px;
+    z-index: 999999;
+  }
+
+.toast{
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    background-color: #fff;
+    border-radius: 5px;
+    border-left: 5px solid;
+    padding: 20px 0;
+    min-width: 400px;
+    max-width: 450px;
+    box-shadow: 0 5px 8px rgba(0, 0, 0, 0.2);
+    transition: all linear 0.3s;
+}
+
+
+
+@keyframes move_ {
+    from{
+        opacity: 0;
+        transform: translateX(calc(100%));
+    }
+
+    to{
+        opacity: 1;
+        transform: translateX(0);
+    }
+}
+
+@keyframes eraser {
+    to{
+        opacity: 0;
+    }
+}
+
+.toast--success{
+    border-color:  green;
+}
+
+.toast--success .toast__icon{
+    color: green;
+}
+
+.toast--info{
+    border-color: blue;
+}
+
+.toast--info .toast__icon{
+    color: blue;
+}
+
+.toast--warning{
+    border-color: orange;
+}
+
+.toast--warning .toast__icon{
+    color: orange;
+}
+
+.toast--error{
+    border-color: red;
+}
+
+.toast--error .toast__icon{
+    color: red;
+}
+.toast + .toast{
+    margin-top: 24px;
+}
+
+.toast__icon, .toast__close{
+    padding: 0 16px;
+}
+
+.toast__body{
+    flex-grow: 1;
+}
+
+.toast__title{
+    font-weight: 600;
+    font-size: 16px;
+    color: #333;
+}
+
+.toast__msg{
+    font-size: 20px;
+    color: #888;
+    margin-top: 6px;
+    line-height: 1.5;
+}
+
+.toast__close{
+    font-size: 20px;
+    color: rgba(0, 0, 0, 0.3);
+    cursor: pointer;
+}
+
+.toast__close:hover{
+    opacity: 0.5;
+}
+
+.bt-box{
+    
+    position: fixed;
+    left: 50%;
+    bottom: 100px;
+    display: flex;
+    transform: translateX(-50%);
+
+}
+
+.bt{
+    display: inline-block;
+    text-decoration: none;
+    background-color: transparent;
+    border: none;
+    outline: none;
+    color: #fff;
+    padding: 12px 48px;
+    border-radius: 50px;
+    cursor: pointer;
+    min-width: 300px;
+    transition: opacity 0.2s ease;
+}
+
+
+
+.bt:hover{
+    opacity: 0.8;
+}
+
+.bt + .bt{
+    margin-left: 15px;
+}
+
+.bt--success{
+    background-color: green;
+    color: #fff;
+}
+
+.bt--error{
+    background-color: red;
+    color: #fff;
+}
+
+.bt--disable{
+    opacity: 0.5 !important;
+    cursor: pointer;
+}
+
+@media only screen and (max-width: 600px) {
+    .bt{
+        min-width: 30%;
+    }
+    .toast{
+        min-width: 200px;
+
+    }
+}
+
+
+
+    </style>
 </head>
 
 <body>
